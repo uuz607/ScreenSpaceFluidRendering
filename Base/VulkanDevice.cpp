486a8f1eec8158fdb6f7678259d436e4e459cc0e
607 <33770576+uuz607@users.noreply.github.com>
2026-04-02 10:24:49 +0800

    Modify
    Updated copyright notice to reflect modifications by Liu Linqi.

--- Base/VulkanDevice.cpp
+++ Base/VulkanDevice.cpp
@@ -1,15 +1,14 @@
 /*
- * Vulkan device class
- * 
- * Encapsulates a physical Vulkan device and its logical representation
- *
- * Copyright (C) 2016-2024 by Sascha Willems - www.saschawillems.de
- *
- * This code is licensed under the MIT license (MIT) (http://opensource.org/licenses/MIT)
- *
- * Modifications Copyright (c) 2026 Liu Linqi
- */
-
+* Vulkan device class
+* 
+* Encapsulates a physical Vulkan device and its logical representation
+*
+* Copyright (C) 2016-2024 by Sascha Willems - www.saschawillems.de
+*
+* This code is licensed under the MIT license (MIT) (http://opensource.org/licenses/MIT)
+*
+*/
+//Modified by Liu Linqi 2026
 
 #if (defined(VK_USE_PLATFORM_IOS_MVK) || defined(VK_USE_PLATFORM_MACOS_MVK) || defined(VK_USE_PLATFORM_METAL_EXT))
 // SRS - Enable beta extensions and make VK_KHR_portability_subset visible
